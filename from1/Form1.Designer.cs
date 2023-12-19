beb04e90076b649d4f280fa8546e3117a2b7f928
sarawut <sarawutkai99@gmail.com>
2023-12-19 11:17:27 +0700

    Add project files.

---
+++ from1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace from1
+{
+    partial class หน้าหลัก
+
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(หน้าหลัก));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.BackColor = Color.FromArgb(255, 128, 128);
+            label1.ForeColor = Color.Blue;
+            label1.Name = "label1";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.BackColor = Color.FromArgb(255, 128, 255);
+            label2.ForeColor = Color.White;
+            label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.BackColor = Color.FromArgb(255, 255, 128);
+            label3.ForeColor = Color.Red;
+            label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.BackColor = Color.Black;
+            label4.ForeColor = Color.White;
+            label4.Name = "label4";
+            label4.Click += label4_Click;
+            // 
+            // หน้าหลัก
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Cyan;
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.Fuchsia;
+            Name = "หน้าหลัก";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ from1/Form1.cs
@@ -0,0 +1,25 @@
+namespace from1
+{
+    public partial class 斯橐伺选 : Form
+    {
+        public 斯橐伺选()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
